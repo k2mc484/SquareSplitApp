0d6f0c6656fe8ceb046eededfc1e356697ed6c93
Michael Hall <Kyle_hall@michaels-macbook-pro.local>
2017-07-14 10:45:47 -0500

    Initial Commit

---
+++ SquareSplitTests/SquareSplitTests.swift
@@ -0,0 +1,36 @@
+//
+//  SquareSplitTests.swift
+//  SquareSplitTests
+//
+//  Created by Michael Hall on 7/14/17.
+//  Copyright © 2017 Hall Enterprises. All rights reserved.
+//
+
+import XCTest
+@testable import SquareSplit
+
+class SquareSplitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
