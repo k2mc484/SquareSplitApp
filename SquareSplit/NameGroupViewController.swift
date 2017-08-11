ed18dd44cca3ec16ad5db21f12e83d5c810fd98a
Michael Hall <Kyle_hall@michaels-macbook-pro.local>
2017-08-10 19:47:18 -0500

    GroupName page requires a name to advance to the next page

--- SquareSplit/NameGroupViewController.swift
+++ SquareSplit/NameGroupViewController.swift
@@ -13,11 +13,30 @@ class NameGroupViewController: UIViewController {
     
     @IBOutlet weak var NameField: UITextField!
     @IBAction func next(_ sender: Any) {
+        if(NameField.text == ""){
+            //Display alery message
+            self.displayMyAlertMessage(userMessage: "Group Name is required");
+            return;
+        }
+        else{
         let myVC = storyboard?.instantiateViewController(withIdentifier: "searchFriends") as! searchFriends
         myVC.groupNameField = NameField.text!
         navigationController?.pushViewController(myVC, animated: true)
+        }
+            
+        
+        
     }
-    
+    func displayMyAlertMessage(userMessage:String)
+    {
+        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title:"Ok", style:UIAlertActionStyle.default, handler:nil);
+        
+        myAlert.addAction(okAction);
+        self.present(myAlert, animated:true , completion:nil);
+    }
+
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
